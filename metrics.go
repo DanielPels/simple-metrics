0146e0ab275db4e1a7f9df8f6ed6aa5fb9978582
Daniël Pels <daniel@gobletgames.com>
2018-01-18 14:05:13 +0100

    Added new EventValue, Improved test.

--- metrics.go
+++ metrics.go
@@ -9,12 +9,17 @@ import (
 )
 
 var events map[string]interface{}
-var eventChan chan string
+var eventChan chan interface{}
 var mu sync.Mutex
+
+type eventValue struct {
+	e string
+	v float64
+}
 
 func init() {
 	events = make(map[string]interface{})
-	eventChan = make(chan string, 1000)
+	eventChan = make(chan interface{}, 1000)
 	mu = sync.Mutex{}
 	go func() {
 		for {
@@ -29,31 +34,49 @@ func init() {
 	}()
 }
 
-func addEvent(e string) {
+func addEvent(e interface{}) {
 	mu.Lock()
 	defer mu.Unlock()
 
-	split := strings.Split(e, ":")
+	isStruct := reflect.TypeOf(e).Kind() == reflect.Struct
+
+	var split []string
+
+	if isStruct {
+		split = strings.Split(e.(eventValue).e, ":")
+	} else {
+		split = strings.Split(e.(string), ":")
+	}
 
 	if len(split) == 1 {
-		if events[split[0]] == nil {
-			events[split[0]] = int(0)
+		if isStruct {
+			if events[split[0]] == nil {
+				events[split[0]] = make([]float64, 0)
+			}
+			if isSliceFloat(events[split[0]]) {
+				events[split[0]] = append(events[split[0]].([]float64), e.(eventValue).v)
+				return
+			}
+		} else {
+			if events[split[0]] == nil {
+				events[split[0]] = int(0)
+			}
+			if isInt(events[split[0]]) {
+				i := events[split[0]].(int)
+				i++
+				events[split[0]] = i
+				return
+			}
 		}
-		if isInt(events[split[0]]) {
-			i := events[split[0]].(int)
-			i++
-			events[split[0]] = i
-			return
-		}
-		fmt.Println("unable to set int, already map - event: " + split[0])
+		fmt.Println("unable to set int or slice, already map - event: " + split[0])
 		return
 	}
 
 	if events[split[0]] == nil {
 		events[split[0]] = make(map[string]interface{})
 	}
-	if isInt(events[split[0]]) {
-		fmt.Println("unable to set map, already int - event: " + split[0])
+	if isInt(events[split[0]]) || isSliceFloat(events[split[0]]) {
+		fmt.Println("unable to set map, already int or slice - event: " + split[0])
 		return
 	}
 
@@ -62,18 +85,27 @@ func addEvent(e string) {
 	for key := 1; key < len(split); key++ {
 		value := split[key]
 		if key == len(split)-1 {
-			if entry[value] == nil {
-				entry[value] = int(0)
+			if isStruct {
+				if entry[value] == nil {
+					entry[value] = make([]float64, 0)
+				}
+				if isSliceFloat(entry[value]) {
+					entry[value] = append(entry[value].([]float64), e.(eventValue).v)
+					return
+				}
+			} else {
+				if entry[value] == nil {
+					entry[value] = int(0)
+				}
+				if isInt(entry[value]) {
+					i := entry[value].(int)
+					i++
+					entry[value] = i
+					return
+				}
 			}
-			if isInt(entry[value]) {
-				i := entry[value].(int)
-				i++
-				entry[value] = i
-				return
-			}
-			fmt.Println("unable to set int, already map - event: " + value)
+			fmt.Println("unable to set int or slice, already map - event: " + value)
 			return
-
 		} else {
 			if entry[value] == nil {
 				entry[value] = make(map[string]interface{})
@@ -82,45 +114,51 @@ func addEvent(e string) {
 				entry = entry[value].(map[string]interface{})
 				continue
 			}
-			fmt.Println("unable to set map, already int - event: " + value)
+			fmt.Println("unable to set map, already int or slice - event: " + value)
 			return
 		}
 	}
 }
 
 func isMapInterface(obj interface{}) bool {
-	if isInt(obj) {
-		return false
-	}
 	t := reflect.TypeOf(obj)
 	if t.Kind() == reflect.Map {
-		if t.Elem().Kind() == reflect.Interface {
-			return true
-		}
+		return t.Elem().Kind() == reflect.Interface
+	}
+	return false
+}
+
+func isSliceFloat(obj interface{}) bool {
+	t := reflect.TypeOf(obj)
+	if t.Kind() == reflect.Slice {
+		return t.Elem().Kind() == reflect.Float64
 	}
 	return false
 }
 
 func isInt(obj interface{}) bool {
-	if reflect.TypeOf(obj).Kind() == reflect.Int {
-		return true
-	}
-	return false
+	return reflect.TypeOf(obj).Kind() == reflect.Int
 }
 
+//Event adds +1 to event. Use ":" as separator to add depth to events. Example: ("Hello:World:love:kittens")
+//Cannot overwrite event that is already a value. Don't do this: ("Hello:World"),("Hello:World:Dog")
 func Event(e string) {
 	eventChan <- e
+}
+
+//EventValue adds a value to event, will be saved in a slice of values. Example: ("User:Score", 134)
+//can be used to calculate average or total sum
+func EventValue(e string, value float64) {
+	eventChan <- eventValue{
+		e: e,
+		v: value,
+	}
 }
 
 func ExportJson() ([]byte, error) {
 	mu.Lock()
 	defer mu.Unlock()
-
-	b, err := json.Marshal(events)
-	if err != nil {
-		return nil, err
-	}
-	return b, nil
+	return json.Marshal(events)
 }
 
 func ClearEvents() {
--- metrics_test.go
+++ metrics_test.go
@@ -5,60 +5,73 @@ import (
 	"testing"
 )
 
+const sleepTime = 50
+
 func TestEvent(t *testing.T) {
+	expectedResult := string(`{"test":1}`)
 
 	ClearEvents()
 
 	Event("test")
 
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * sleepTime)
 
 	b, err := ExportJson()
 	if err != nil {
-		panic(err)
+		t.Error("TestEvent: export json")
+		return
 	}
-	if string(b) != string(`{"test":1}`) {
-		t.Fail()
+
+	if string(b) != expectedResult {
+		t.Error("TestEvent invalid return. Got: " + string(b) + " Should be: " + expectedResult)
 	}
 }
 
 func TestMultipleOfOneEvent(t *testing.T) {
+	expectedResult := string(`{"test":3}`)
+
 	ClearEvents()
 
 	Event("test")
 	Event("test")
 	Event("test")
 
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * sleepTime)
 
 	b, err := ExportJson()
 	if err != nil {
-		panic(err)
+		t.Error("TestMultipleOfOneEvent: export json")
+		return
 	}
 
-	if string(b) != string(`{"test":3}`) {
-		t.Fail()
+	if string(b) != expectedResult {
+		t.Error("TestMultipleOfOneEvent invalid return. Got: " + string(b) + " Should be: " + expectedResult)
 	}
 }
 
 func TestMultipleDepthEvent(t *testing.T) {
+	expectedResult := string(`{"depth1":{"depth2":{"depth3":{"depth":{"4":{"depth5":{"depth6":{"depth7":{"depth8":{"depth9":{"depth10":1}}}}}}}}}}}`)
+
 	ClearEvents()
 
 	Event("depth1:depth2:depth3:depth:4:depth5:depth6:depth7:depth8:depth9:depth10")
 
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * sleepTime)
 
 	b, err := ExportJson()
 	if err != nil {
-		panic(err)
+		t.Error("TestMultipleDepthEvent: export json")
+		return
 	}
 
-	if string(b) != string(`{"depth1":{"depth2":{"depth3":{"depth":{"4":{"depth5":{"depth6":{"depth7":{"depth8":{"depth9":{"depth10":1}}}}}}}}}}}`) {
-		t.Fail()
+	if string(b) != expectedResult {
+		t.Error("TestMultipleDepthEvent invalid return. Got: " + string(b) + " Should be: " + expectedResult)
 	}
 }
 
 func TestMultipleDepthOfOneEvent(t *testing.T) {
+	expectedResult := string(`{"depth1":{"depth2":{"depth3":{"depth":{"4":{"depth5":{"depth6":{"depth7":{"depth8":{"depth9":{"depth10":5}}}}}}}}}}}`)
+
 	ClearEvents()
 
 	Event("depth1:depth2:depth3:depth:4:depth5:depth6:depth7:depth8:depth9:depth10")
@@ -67,14 +80,132 @@ func TestMultipleDepthOfOneEvent(t *testing.T) {
 	Event("depth1:depth2:depth3:depth:4:depth5:depth6:depth7:depth8:depth9:depth10")
 	Event("depth1:depth2:depth3:depth:4:depth5:depth6:depth7:depth8:depth9:depth10")
 
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * sleepTime)
 
 	b, err := ExportJson()
 	if err != nil {
-		panic(err)
+		t.Error("TestMultipleDepthOfOneEvent: export json")
+		return
 	}
 
-	if string(b) != string(`{"depth1":{"depth2":{"depth3":{"depth":{"4":{"depth5":{"depth6":{"depth7":{"depth8":{"depth9":{"depth10":5}}}}}}}}}}}`) {
-		t.Fail()
+	if string(b) != expectedResult {
+		t.Error("TestMultipleDepthOfOneEvent invalid return. Got: " + string(b) + " Should be: " + expectedResult)
+	}
+}
+
+func TestEventValue(t *testing.T) {
+	expectedResult := string(`{"test":[20.38]}`)
+
+	ClearEvents()
+
+	EventValue("test", 20.38)
+
+	time.Sleep(time.Millisecond * sleepTime)
+
+	b, err := ExportJson()
+	if err != nil {
+		t.Error("TestEventValue: export json")
+		return
+	}
+
+	if string(b) != expectedResult {
+		t.Error("TestEventValue invalid return. Got: " + string(b) + " Should be: " + expectedResult)
+	}
+}
+
+func TestMultipleOfOneEventValue(t *testing.T) {
+	expectedResult := string(`{"test":[42.38,-0.38,32847.38]}`)
+
+	ClearEvents()
+
+	EventValue("test", 42.38)
+	EventValue("test", -0.38)
+	EventValue("test", 32847.38)
+
+	time.Sleep(time.Millisecond * sleepTime)
+
+	b, err := ExportJson()
+	if err != nil {
+		t.Error("TestMultipleOfOneEventValue: export json")
+		return
+	}
+
+	if string(b) != expectedResult {
+		t.Error("TestMultipleOfOneEventValue invalid return. Got: " + string(b) + " Should be: " + expectedResult)
+	}
+}
+
+func TestMultipleDepthOfOneEventValue(t *testing.T) {
+	expectedResult := string(`{"dept1":{"depth2":{"depth3":[947.82]}}}`)
+
+	ClearEvents()
+
+	EventValue("dept1:depth2:depth3", 947.82)
+
+	time.Sleep(time.Millisecond * sleepTime)
+
+	b, err := ExportJson()
+	if err != nil {
+		t.Error("TestMultipleDepthOfOneEventValue: export json")
+		return
+	}
+
+	if string(b) != expectedResult {
+		t.Error("TestMultipleDepthOfOneEventValue invalid return. Got: " + string(b) + " Should be: " + expectedResult)
+	}
+}
+
+func TestMultipleDepthOfMultipleEventValue(t *testing.T) {
+	expectedResult := string(`{"dept1":{"depth2":{"depth3":[2987.7,-9283,387.2]}}}`)
+
+	ClearEvents()
+
+	EventValue("dept1:depth2:depth3", 2987.7)
+	EventValue("dept1:depth2:depth3", -9283)
+	EventValue("dept1:depth2:depth3", 387.2)
+
+	time.Sleep(time.Millisecond * sleepTime)
+
+	b, err := ExportJson()
+	if err != nil {
+		t.Error("TestMultipleDepthOfMultipleEventValue: export json")
+		return
+	}
+
+	if string(b) != expectedResult {
+		t.Error("TestMultipleDepthOfMultipleEventValue invalid return. Got: " + string(b) + " Should be: " + expectedResult)
+	}
+}
+
+func TestFinal(t *testing.T) {
+	expectedResult := string(`{"depth1":{"depth2":2,"other":2},"hello":1,"life":[42,-180],"test":2,"valueDepth1":{"other1":[872,-48],"valueDepth2":[-99,273]}}`)
+
+	ClearEvents()
+
+	Event("hello")
+	Event("test")
+	Event("test")
+	Event("depth1:depth2")
+	Event("depth1:depth2")
+	Event("depth1:other")
+	Event("depth1:other")
+
+	EventValue("life", 42)
+	EventValue("life", -180)
+	EventValue("valueDepth1:valueDepth2", -99)
+	EventValue("valueDepth1:valueDepth2", 273)
+	EventValue("valueDepth1:other1", 872)
+	EventValue("valueDepth1:other1", -48)
+
+	time.Sleep(time.Millisecond * sleepTime)
+
+	b, err := ExportJson()
+	if err != nil {
+		t.Error("TestMultipleDepthOfMultipleEventValue: export json")
+		return
+	}
+
+	if string(b) != expectedResult {
+		t.Error("TestMultipleDepthOfMultipleEventValue invalid return. Got: " + string(b) + " Should be: " + expectedResult)
 	}
 }
